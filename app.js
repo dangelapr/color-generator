2140ec24687e58735cdecb6084e31b01081d49c5
D'Angela Pritchett-Rowe <dangela.prit.rowe@gmail.com>
2021-06-23 20:51:55 -0400

    trying to make more accessible by testing the text color against bg color for proper contrast and changing it to black/white when necessary

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 let button = document.querySelector('button');
 let h1 = document.body.querySelector('h1');
 
-button.addEventListener('click', function(){
+function randomBG() {
     let r = Math.floor(Math.random()*256);
     let g = Math.floor(Math.random()*256);
     let b = Math.floor(Math.random()*256);
-    let bgColor = `rgb(${r}, ${g}, ${b})`;
+    return `rgb(${r}, ${g}, ${b})`;
+};
+
+
+// i think i need to take the result from randomBG(), which will be rgb(#, #, #) as a string, and slice it into 3 numbers and reassign them to r, g, and b in order for the getContrastYIQ() to work.
+
+function getContrastYIQ() {
+    let yiq = ((r*299) + (g*587) + (b*114)) / 1000;
+    return (yiq >= 500) ? 'black' : 'white';
+};
+
+button.addEventListener('click', function() {
+    let bgColor = randomBG();
     document.body.style.backgroundColor = bgColor;
-    h1.textContent = `this color is ${bgColor}`
+    let textColor = getContrastYIQ(bgColor);
+    document.body.style.color = textColor;
+    h1.textContent = `this color is ${bgColor}`;
 });
+
